bb9aabc5e99a30632abdc6533fe37acc835b14bb
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2024-01-05 16:41:48 -0800

    youtube video downloader

--- youtube.py
+++ youtube.py
@@ -1,16 +1,35 @@
 from pytube import YouTube
+from pytube import Playlist
+import os
+
+p = Playlist(
+    'https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ')
 
 
-def download(link):
+def download(link, playlist_count):
     try:
         youtubeObject = YouTube(link)
         resolution = youtubeObject.streams.get_highest_resolution()
-        resolution.download()
+        print("Downloading: " + youtubeObject.title)
+        filepath = 'C:/Users/Vishnu/Desktop/Videos/'
+        resolution.download(filepath)
+
+        filename = resolution.default_filename
+        updated_filename = str(playlist_count)+"_"+filename
+        os.rename(os.path.join(filepath, filename),
+                  os.path.join(filepath, updated_filename))
 
     except:
         print("Error")
 
-    print("Download complete!")
+    print("Download complete!: " + updated_filename)
 
 
-download("https://www.youtube.com/watch?v=6UQZ5oQg8XA")
+# download("https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ")
+
+
+playlist_count = 34
+for url in p.video_urls[33:len(p.video_urls)]:
+    # print(url)
+    download(url, playlist_count)
+    playlist_count += 1
